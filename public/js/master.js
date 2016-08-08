794e6cca9d0ab824e51b3b366f7761c71e0c97c4
Angel Vanegas <angelman@gmail.com>
2016-08-08 14:10:42 -0400

    figuring out tiles

--- public/js/master.js
+++ public/js/master.js
@@ -1,9 +1,10 @@
 var instr = {};
-instr.vol = new Tone.Gain(0.8).toMaster();
+instr.vol = new Tone.Gain(0.0).toMaster();
 instr.crush = new Tone.BitCrusher(3).connect(instr.vol);
 instr.sound = new Tone.MonoSynth().connect(instr.crush);
 
 nx.onload = function(){
+  nx.colorize("lightblue");
   knob.on('*', function(){
     instr.vol.gain.value = this.val.value;
   })
