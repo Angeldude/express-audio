29713d82cf5870d3fb09933282a7dea80fff1387
Angel Vanegas <angelman@gmail.com>
2016-08-09 17:02:42 -0400

    content change

--- public/js/master.js
+++ public/js/master.js
@@ -1,12 +1,14 @@
 var instr = {};
 instr.rand = function(){ return (Math.floor(Math.random() * 35) + 40)};
-instr.vol = new Tone.Gain(0.0).toMaster();
+instr.vol = new Tone.Gain(0.5).toMaster();
 instr.sound = new Tone.FMSynth().connect(instr.vol);
 
 nx.onload = function(){
   nx.colorize("lightblue");
-  slide.val.value = 0;
+  slide.val.value = 0.5;
   slide.draw();
+  banner1.colors.accent = "#6d3480";
+  banner1.draw();
   slide.on('*', function(){
     instr.vol.gain.value = this.val.value;
   })
