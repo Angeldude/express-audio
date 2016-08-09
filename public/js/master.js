fdbaba83a704c5e0d0cdcf3d20c6fe35ed79d85e
Angel Vanegas <angelman@gmail.com>
2016-08-09 15:57:26 -0400

    changes i cant remember

--- public/js/master.js
+++ public/js/master.js
@@ -1,15 +1,16 @@
 var instr = {};
-instr.mtof = function(midi){ return (440 * (Math.pow(2,(midi - 69)/12)))};
 instr.rand = function(){ return (Math.floor(Math.random() * 35) + 40)};
 instr.vol = new Tone.Gain(0.0).toMaster();
 instr.sound = new Tone.FMSynth().connect(instr.vol);
 
 nx.onload = function(){
   nx.colorize("lightblue");
-  knob.on('*', function(){
+  slide.val.value = 0;
+  slide.draw();
+  slide.on('*', function(){
     instr.vol.gain.value = this.val.value;
   })
   signal.on('*', function(){
-    instr.sound.triggerAttackRelease(instr.mtof(instr.rand()), "8n");
+    instr.sound.triggerAttackRelease(nx.mtof(instr.rand()), "8n");
   })
 }
