55a0e8c984822ddd12538e619efffb182751a694
Angel Vanegas <angelman@gmail.com>
2016-09-07 01:50:15 -0400

    added pattern and toggle

--- public/js/master.js
+++ public/js/master.js
@@ -1,14 +1,28 @@
 var instr = {};
-instr.rand = function(){ return (Math.floor(Math.random() * 35) + 40)};
+instr.rand = function(){ return (Math.floor(Math.random() * 6))};
 instr.vol = new Tone.Gain(0.5).toMaster();
-instr.sound = new Tone.FMSynth().connect(instr.vol);
+instr.reverb = new Tone.JCReverb(0.4);
+instr.delay = new Tone.FeedbackDelay(0.5).connect(instr.vol);
+instr.sound = new Tone.PolySynth().chain(instr.reverb, instr.delay);
+
+instr.synth = new Tone.Synth();
+instr.synth.toMaster();
+var pattern = new Tone.Pattern(function(time, note){
+  instr.synth.triggerAttackRelease(note, 0.15);
+}, ["C4", "Eb4", "G4", "Ab4"]);
+
+instr.test = ["D5", "C5","Ab4","F5", "Eb5", "G4"];
+
+Tone.Transport.bpm.value = 340;
+pattern.start(0);
+
 
 nx.onload = function(){
   nx.colorize("lightblue");
   slide.val.value = 0.5;
   slide.draw();
-  banner1.colors.accent = "#6d3480";
-  banner1.draw();
+  // banner1.colors.accent = "#6d3480";
+  // banner1.draw();
   switched.colors.border = "white";
   switched.draw();
 
@@ -16,13 +30,13 @@ nx.onload = function(){
     instr.vol.gain.value = this.val.value;
   })
   signal.on('*', function(){
-    instr.sound.triggerAttackRelease(nx.mtof(instr.rand()), "8n");
+    instr.sound.triggerAttackRelease(instr.test[instr.rand()], "1n");
   })
   switched.on('*', function(){
     if(switched.val.value === 1){
-      console.log("i'm on!");
+      Tone.Transport.start();
     } else
-      console.log("i'm off");
+      Tone.Transport.stop();
   })
 
 }
