0543c600d8a7b176808426c9fe3f05c6b5cb9993
Angel Vanegas <angelman@gmail.com>
2016-09-07 23:07:03 -0400

    new stuff

--- public/js/master.js
+++ public/js/master.js
@@ -1,13 +1,33 @@
 var instr = {};
-instr.rand = function(){ return (Math.floor(Math.random() * 35) + 40)};
-instr.vol = new Tone.Gain(0.5).toMaster();
-instr.sound = new Tone.FMSynth().connect(instr.vol);
 
+// EFFECTS SETUP
+instr.vol = new Tone.Gain(0.5).toMaster();
+instr.reverb = new Tone.JCReverb(0.4);
+instr.delay = new Tone.FeedbackDelay(0.5).connect(instr.vol);
+
+//  INSTRUMENT SETUP
+instr.rand = function(){ return (Math.floor(Math.random() * 7))};
+instr.sound = new Tone.PolySynth(6, Tone.Synth).chain(instr.reverb, instr.delay);
+instr.synth = new Tone.Synth().toMaster();
+
+// MUSIC LOGIC
+var pattern = new Tone.Pattern(function(time, note){
+  instr.synth.triggerAttackRelease(note, "8n");
+}, ["C4", "Eb4", "G4", "Ab4"], "alternateUp");
+
+instr.test = ["D5", "C5", "Ab4", "C4", "F5", "Eb5", "G4"];
+instr.delay.wet.value = 0.4;
+instr.reverb.wet.value = 0.5;
+
+Tone.Transport.bpm.value = 240;
+pattern.start(0);
+
+// UI STYLING
 nx.onload = function(){
   nx.colorize("lightblue");
   slide.val.value = 0.5;
   slide.draw();
-  banner1.colors.accent = "#6d3480";
+  banner1.colors.accent = "#6d1480";
   banner1.draw();
   switched.colors.border = "white";
   switched.draw();
@@ -16,13 +36,13 @@ nx.onload = function(){
     instr.vol.gain.value = this.val.value;
   })
   signal.on('*', function(){
-    instr.sound.triggerAttackRelease(nx.mtof(instr.rand()), "8n");
+    instr.sound.triggerAttackRelease(instr.test[instr.rand()], "1n");
   })
   switched.on('*', function(){
     if(switched.val.value === 1){
-      console.log("i'm on!");
+      Tone.Transport.start();
     } else
-      console.log("i'm off");
+      Tone.Transport.stop();
   })
 
 }
